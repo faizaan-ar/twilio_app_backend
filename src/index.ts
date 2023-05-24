1e39942466e348074c6c3bbbd2fc60076edb34fa
Faizaan Arshad <faizaan@banjaxed.com>
2023-05-24 15:21:13 -0500

    initialize router

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import { createDBConnect } from './sequelize/connection';
 import { AccountModel } from './sequelize/models/account.model';
+import { routes } from './routes/index.routes';
 
 const app = express();
 
 createDBConnect();
+
+app.use('/api', routes);
 
 app.listen(3000, () => {
     console.log("Server running on port http://localhost:3000");
